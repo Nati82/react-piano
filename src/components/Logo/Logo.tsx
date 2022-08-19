b7f8ef291fa9dde415a945f738d57f26aabf8384
Nati82 <ashenafinati82@gmail.com>
2022-08-19 15:53:13 +0300

    added emojis

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,13 +4,13 @@ export const Logo = () => {
   return (
     <h1 className={styles.logo}>
       <span role="img" aria-label="metal hand emoji">
-        ￿
+      &#129304;
       </span>
       <span role="img" aria-label="musical keyboard emoji">
-        ￿
+      &#127929;
       </span>
       <span role="img" aria-label="musical notes emoji">
-        ￿
+      &#127926;
       </span>
     </h1>
   );
